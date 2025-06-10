0e85399aa62f0615e401cec5b8410e7896773018
Kristian Volarevic <kristianvola@gmail.com>
2025-06-10 12:41:12 +1000

    Component Created & Styling Started
    - Created the header component using angular.
    - Started styling the header using bootstrap and google fonts.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'Portfolio';
