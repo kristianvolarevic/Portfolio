453935de242748d0aed86fea1e03bc58e0c287ed
Kristian Volarevic <kristianvola@gmail.com>
2025-06-13 15:08:58 +1000

    About Page - Introduction & Sections

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -4,8 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   imports: [],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css',
 })
 export class About {
+  sections = [
+    {
+      title: 'Work History',
+      expanded: false,
+      items: ['Crew Member at Guzman y Gomez Burnside (2023-2025)'],
+    },
+    {
+      title: 'Qualifications',
+      expanded: false,
+      items: [
+        "Bachelor's Degree of Information Technology, Monash University (2025)",
+      ],
+    },
+    {
+      title: 'Skills & Tools',
+      expanded: false,
+      items: ['Test1'],
+    },
+  ];
 
+  toggleSection(section: any) {
+    section.expanded = !section.expanded;
+  }
 }
