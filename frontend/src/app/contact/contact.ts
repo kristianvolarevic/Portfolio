47eddbc785c8b8b40c48322bbd6840a64325656a
Kristian Volarevic <kristianvola@gmail.com>
2025-06-27 09:26:19 +1000

    Spinner & Custom Message
    - Added a spinner to show that the mesage is being sent.
    - Added a custom success popup and removed alert.
    - Added a custom error popup and removed alert.

--- frontend/src/app/contact/contact.ts
+++ frontend/src/app/contact/contact.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ContactService } from '../contact-service';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +12,10 @@ import { ContactService } from '../contact-service';
 })
 export class ContactComponent {
   contactForm: FormGroup;
+  isLoading: boolean = false;
+
+  @ViewChild('successModal') successModal!: ElementRef;
+  @ViewChild('errorModal') errorModal!: ElementRef;
 
   constructor(private fb: FormBuilder, private contactService: ContactService) {
     this.contactForm = this.fb.group({
@@ -21,9 +27,27 @@ export class ContactComponent {
   }
 
   onSubmit() {
+    if (this.contactForm.invalid) {
+      alert('Fill all fields marked with *');
+      return;
+    }
+
+    this.isLoading = true;
+
     this.contactService.sendFormData(this.contactForm.value).subscribe({
-      next: (res) => alert('Message sent'),
-      error: (err) => alert('Error sending message'),
+      next: (res) => {
+        this.isLoading = false;
+        this.contactForm.reset();
+
+        const modal = new bootstrap.Modal(this.successModal.nativeElement);
+        modal.show();
+      },
+      error: (err) => {
+        this.isLoading = false;
+
+        const modal = new bootstrap.Modal(this.errorModal.nativeElement);
+        modal.show();
+      },
     });
   }
 }
