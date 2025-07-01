f4371a702c8abfef1af6e9ba91f3bbd178665d8c
Kristian Volarevic <kristianvola@gmail.com>
2025-07-02 09:24:27 +1000

    Error 404 Page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Projects } from './projects/projects';
 import { ContactComponent } from './contact/contact';
 import { About } from './about/about';
 import { Project } from './project/project';
+import { Error404 } from './error404/error404';
 
 export const routes: Routes = [
   {
@@ -15,4 +16,7 @@ export const routes: Routes = [
   { path: 'projects', component: Projects },
   { path: 'contact', component: ContactComponent },
   { path: 'projects/:id', component: Project },
+
+  /* Must be the last route */
+  { path: '**', component: Error404 },
 ];
